9498a1d41f3191d03bbf5db444c313b9f04cbaf7
tesscat <tesscat.cat@gmail.com>
2024-02-04 21:29:08 +0000

    fixed it

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './index.css'
+import '../app/globals.css'
 import Recipe, {loader as recipeLoader} from './components/Recipe.tsx';
 import React from 'react';
 import PageHandler from './PageHandler.tsx';
